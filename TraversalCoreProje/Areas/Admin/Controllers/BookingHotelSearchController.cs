4302eec27a17844375de689346424eb1ece3ffbf
mehmet kayıkcı <06mehmetkayikci@gmail.com>
2024-11-06 14:48:48 +0300

    Booking hotel search eklenmesi

---
+++ TraversalCoreProje/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TraversalCoreProje.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class BookingHotelSearchController : Controller
+    {
+        public async Task <IActionResult> Index()
+        {
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?dest_id=-1456928&order_by=popularity&checkout_date=2025-01-19&children_number=2&filter_by_currency=EUR&locale=en-gb&dest_type=city&checkin_date=2025-01-18&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&include_adjacency=true&page_number=0&adults_number=2&room_number=1&units=metric"),
+                Headers =
+    {
+        { "x-rapidapi-key", "f9d4528dcamsh7ea0bc35df8c60ep1d0fa6jsn5166d805f55e" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var bodyReplace = body.Replace(".", "");
+                var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(bodyReplace);
+                return View(values.results);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetCityDestID()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> GetCityDestID(string p)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=en-gb&name={p}"),
+                Headers =
+    {
+        { "x-rapidapi-key", "f9d4528dcamsh7ea0bc35df8c60ep1d0fa6jsn5166d805f55e" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                return View();
+            }
+        }
+    }
+}
