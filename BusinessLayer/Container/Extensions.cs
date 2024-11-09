6e28f5a0890354cb0058d1de4fb441b5b82ba36d
mehmet kayıkcı <06mehmetkayikci@gmail.com>
2024-11-09 23:39:50 +0300

    Unit Of Work

--- BusinessLayer/Container/Extensions.cs
+++ BusinessLayer/Container/Extensions.cs
@@ -1,8 +1,11 @@
 ﻿using BusinessLayer.Abstract;
+using BusinessLayer.Abstract.AbstractUow;
 using BusinessLayer.Concrete;
+using BusinessLayer.Concrete.ConcreteUow;
 using BusinessLayer.ValidationRules;
 using DataAccessLayer.Abstract;
 using DataAccessLayer.EntityFramework;
+using DataAccessLayer.UnitOfWork;
 using DTOLayer.DTOs.AnnoncementDTOs;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
@@ -41,6 +44,11 @@ namespace BusinessLayer.Container
 
             services.AddScoped<IAnnoncementService, AnnoncementManager>();
             services.AddScoped<IAnnoncementDal, EfAnnoncementDal>();
+
+            services.AddScoped<IAccountService,AccountManager>();
+            services.AddScoped<IAccountDal,EfAccountDal>();
+
+            services.AddScoped<IUowDal,UowDal>();
 
         }
 
